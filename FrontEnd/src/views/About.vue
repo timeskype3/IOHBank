610a47fd2a20cacb7a570cd93d65ac5dd0884d7a
Jirayu Janraluek <mewti@hotmail.com>
2019-05-23 12:48:36 +0700

    Test2

--- FrontEnd/src/views/About.vue
+++ FrontEnd/src/views/About.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="about">
-<<<<<<< HEAD
-    <h1>Mr.Jirayu Janraluek</h1>
-=======
-    <h1>This is an about page</h1>
->>>>>>> parent of 0d8eb6c... ChangName
-  </div>
+    <p>Time</p>
+    </div>
 </template>
