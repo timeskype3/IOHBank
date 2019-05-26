00586a609b976f6c3d3e7e9a811caecd37ccb6d4
Jirayu Janraluek <mewti@hotmail.com>
2019-05-26 13:00:04 +0700

    Install JWT

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,7 +2,9 @@ const express=require("express");
 const cors=require("cors");
 const bodyParser=require("body-parser")
 const mysql = require('mysql')
+const jwt = require('jsonwebtoken')
 const app=express()
+
 app.use(cors())
 app.use(bodyParser.json())
 
@@ -14,6 +16,7 @@ const conn = mysql.createConnection({
 })
 
 conn.connect()
+
 
 app.post("/Login",(req,res)=>{
     conn.query('SELECT FName, LName, Username, Password FROM clientinfo WHERE Username = ? AND Password = ?', [
@@ -35,6 +38,53 @@ app.post("/Login",(req,res)=>{
         // }
     })
 })
+
+/*app.post('/api/dashboard',verifyToken,(req,res)=>{
+    jwt.verify(req.token, 'secretkey',(err, authData)=>{
+        if(err){
+            res.sendStatus(404);
+        }else{
+            res.json({
+                message: 'Hello',
+                authData
+            });
+        }
+    });
+});
+
+app.post('/api/login', (req,res)=> {
+    const user={
+        id: 1,
+        username: 'Timeskype3',
+        email: 'mewti@hotmail.com'
+    }
+    jwt.sign({user},'secretkey',(err,token)=>{
+        res.json({
+            token
+        });
+    });
+});
+//Format of Token
+//Authorization : Bearer <access_token>
+
+function verifyToken(req,res,next){
+    //Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    //Check if bearer is undifined
+    if(typeof bearerHeader !== 'undefined'){
+        //Split at the space
+        const bearer = bearerHeader.split(' ');
+        //Get token from array
+        const bearerToken = bearer[1];
+        //Set thetoken
+        req.token = bearerToken;
+        //Next
+        next();
+    } else{
+        //Forbiden
+        res.sendStatus(403);
+    }
+}*/
 
 
 
