3d247b5be8223f0a6a9ebb27993f7198ba76d3db
Jirayu Janraluek <mewti@hotmail.com>
2019-05-26 17:07:15 +0700

    Add Token

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -5,6 +5,8 @@ const mysql = require('mysql')
 const jwt = require('jsonwebtoken')
 const app=express()
 
+const SECRET_KEY = ''
+
 app.use(cors())
 app.use(bodyParser.json())
 
@@ -18,52 +20,51 @@ const conn = mysql.createConnection({
 conn.connect()
 
 
-app.post("/Login",(req,res)=>{
-    conn.query('SELECT FName, LName, Username, Password FROM clientinfo WHERE Username = ? AND Password = ?', [
+// app.post("/Login",(req,res)=>{
+//     conn.query('SELECT FName, LName, Username, Password FROM clientinfo WHERE Username = ? AND Password = ?', [
+//         req.body.user,
+//         req.body.password
+//     ], (err, results) => {
+//         res.json({
+//             user: results
+//         })
+//         // if(results.length === 1) {
+//         //     res.json({
+//         //         name: results[0].FName + ' ' + results[0].LName,
+//         //         status: 'found'
+//         //     })
+//         // } else {
+//         //     res.json({
+//         //         status: 'not found'
+//         //     })
+//         // }
+//     })
+// })
+
+app.post('/dashboard',verifyToken,(req,res)=>{
+    res.json(req.user)
+});
+
+app.post('/login', (req,res)=> {
+    conn.query('SELECT ClientID, FName, LName, Email FROM clientinfo WHERE Username = ? AND Password = ?', [
         req.body.user,
         req.body.password
     ], (err, results) => {
-        res.json({
-            user: results
-        })
-        // if(results.length === 1) {
-        //     res.json({
-        //         name: results[0].FName + ' ' + results[0].LName,
-        //         status: 'found'
-        //     })
-        // } else {
-        //     res.json({
-        //         status: 'not found'
-        //     })
-        // }
-    })
-})
-
-/*app.post('/api/dashboard',verifyToken,(req,res)=>{
-    jwt.verify(req.token, 'secretkey',(err, authData)=>{
-        if(err){
-            res.sendStatus(404);
-        }else{
+        jwt.sign({
+            id: results[0].ClientID,
+            name: [results[0].FName, results[0].LName].join(' '),
+            email: results[0].Email,
+        },'secretkey',(err,token)=>{
             res.json({
-                message: 'Hello',
-                authData
+                token
             });
-        }
-    });
+        });
+    })
 });
 
-app.post('/api/login', (req,res)=> {
-    const user={
-        id: 1,
-        username: 'Timeskype3',
-        email: 'mewti@hotmail.com'
-    }
-    jwt.sign({user},'secretkey',(err,token)=>{
-        res.json({
-            token
-        });
-    });
-});
+app.get('/auth', verifyToken, (req, res) => {
+   res.sendStatus(200)
+})
 //Format of Token
 //Authorization : Bearer <access_token>
 
@@ -76,15 +77,22 @@ function verifyToken(req,res,next){
         const bearer = bearerHeader.split(' ');
         //Get token from array
         const bearerToken = bearer[1];
-        //Set thetoken
-        req.token = bearerToken;
+
+        /* verify */
+        jwt.verify(bearerToken, 'secretkey', (err, authData)=>{
+            if(err){
+                res.sendStatus(403);
+            }else{
+                req.user = authData
+                next();
+            }
+        });
         //Next
-        next();
     } else{
         //Forbiden
         res.sendStatus(403);
     }
-}*/
+}
 
 
 
