7621260165e6d1ac08e0b1422404b19fc3fdbd32
Jariwat Phansri <jariwat557@gmail.com>
2019-05-28 01:08:26 +0700

    Account Profile

--- FrontEnd/src/views/authenticated/Account/Profile.vue
+++ FrontEnd/src/views/authenticated/Account/Profile.vue
@@ -2,46 +2,63 @@
   <div>
     <template>
       <a-card title="Information">
-        <a-card-grid style="width:25%;textAlign:'center'"
-          >Account Name</a-card-grid
-        >
+        <a-card-grid style="width:25%;textAlign:'center'">Account Name</a-card-grid>
         <a-card-grid style="width:75%;textAlign:'center'">{{
           profile.FName + " " + profile.LName
+        }}</a-card-grid>
+
+        <a-card-grid style="width:25%;textAlign:'center'">Date of Birth</a-card-grid> 
+        <a-card-grid style="width:25%;textAlign:'center'">{{ 
+          profile.DateOfBirth.split("T")[0].split("-")[2] + "/" + 
+          profile.DateOfBirth.split("T")[0].split("-")[1] + "/" + 
+          profile.DateOfBirth.split("T")[0].split("-")[0]
+        }}</a-card-grid> 
+
+        <a-card-grid style="width:25%;textAlign:'center'">Age</a-card-grid>
+        <a-card-grid style="width:25%;textAlign:'center'">{{
+          new Date().getFullYear() - profile.DateOfBirth.split("-")[0]
         }}</a-card-grid>
 
         <a-card-grid style="width:25%;textAlign:'center'">Gender</a-card-grid>
         <a-card-grid style="width:25%;textAlign:'center'">{{
           profile.Gender
         }}</a-card-grid>
-        <a-card-grid style="width:25%;textAlign:'center'">Age</a-card-grid>
+
+        <a-card-grid style="width:25%;textAlign:'center'">BloodType</a-card-grid>
         <a-card-grid style="width:25%;textAlign:'center'">{{
-          new Date().getFullYear() - profile.DateOfBirth.split("-")[0]
+          profile.BloodType
         }}</a-card-grid>
 
-        <a-card-grid style="width:25%;textAlign:'center'">Adress</a-card-grid>
-        <a-card-grid style="width:75%;textAlign:'center'">E-mail</a-card-grid>
+        <a-card-grid style="width:25%;textAlign:'center'">Address</a-card-grid>
+        <a-card-grid style="width:75%;textAlign:'center'">{{
+          profile.Address  
+        }}</a-card-grid>
+
+        <a-card-grid style="width:25%;textAlign:'center'">Nationality</a-card-grid>
+        <a-card-grid style="width:25%;textAlign:'center'">{{
+          profile.Nationality
+        }}</a-card-grid>
+
+        <a-card-grid style="width:25%;textAlign:'center'">CitizenID</a-card-grid>
+        <a-card-grid style="width:25%;textAlign:'center'">{{
+          profile.IDCardNumber
+        }}</a-card-grid>
 
         <a-card-grid style="width:25%;textAlign:'center'">E-mail</a-card-grid>
         <a-card-grid style="width:25%;textAlign:'center'">{{
           profile.Email
         }}</a-card-grid>
+
         <a-card-grid style="width:25%;textAlign:'center'">Phone</a-card-grid>
         <a-card-grid style="width:25%;textAlign:'center'">{{
           "0" + profile.Tel
         }}</a-card-grid>
-
-        <a-card-grid style="width:25%;textAlign:'center'"
-          >Nationality</a-card-grid
-        >
-        <a-card-grid style="width:25%;textAlign:'center'">{{
-          profile.Nationality
-        }}</a-card-grid>
       </a-card>
     </template>
 
-    <p>
+    <!-- <p> 
       {{ profile }}
-    </p>
+    </p> -->
   </div>
 </template>
 
