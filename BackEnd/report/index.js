2cf8603bc5d936a03357373efe5edf5c6132fb85
satansaga <mos.nattakul@gmail.com>
2019-05-30 01:55:12 +0700

    report and index

--- BackEnd/report/index.js
+++ BackEnd/report/index.js
@@ -1,14 +1,8 @@
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser")
-const mysql = require('mysql')
-const jwt = require('jsonwebtoken')
 const app = express.Router()
+const mysql = require('mysql')
 
 const SECRET_KEY = ''
-
-app.use(cors())
-app.use(bodyParser.json())
 
 const conn = mysql.createConnection({
     host: 'localhost',
@@ -19,13 +13,181 @@ const conn = mysql.createConnection({
 
 conn.connect()
 
-app.get('/', (req, res) => {
+app.get('/accountcount', (req, res) => {
     conn.query('SELECT c.FName, c.LName, COUNT(*) as CountOfAccount FROM clientinfo c JOIN accountinfo a ON c.ClientID=a.ClientID GROUP BY c.ClientID',
         (err, results) => {
-            if (err) return releaseEvents.sendStatus(500)
+            if (err) return res.status(400).send(String(err))
             res.json(results)
         })
 })
+
+
+app.get('/closecount', (req, res) => {
+
+    conn.query('SELECT COUNT(*) as count FROM closeaccount',
+        (err, results) => {
+            if (err) return res.status(400).send(String(err))
+            res.json(results)
+        })
+})
+
+app.get('/churncount', (req, res) => {
+
+    conn.query('SELECT NewBank, COUNT(*) as count FROM closeaccount GROUP BY NewBank',
+        (err, results) => {
+            if (err) return res.status(400).send(String(err))
+            res.json(results)
+        })
+})
+
+app.get('/transfer', (req, res) => {
+
+    conn.query('SELECT Bank , COUNT(*) as count FROM ordertransaction o JOIN accountinfo a ON o.AccountIDRecive = a.AccountID GROUP BY Bank',
+        (err, results) => {
+            if (err) return res.status(400).send(String(err))
+            res.json(results)
+        })
+})
+
+app.get('/billing', (req, res) => {
+
+    conn.query(`SELECT Type ,COUNT(*) as count FROM billpamentdata WHERE billpamentdata.Status = 'Paid'  GROUP BY Type`,
+        (err, results) => {
+            if (err) return res.status(400).send(String(err))
+            res.json(results)
+        })
+})
+
+app.get('/promo', (req, res) => {
+
+    conn.query(`SELECT Type ,COUNT(*) as count FROM billpamentdata WHERE billpamentdata.Status = 'Paid'  GROUP BY Type`,
+        (err, results) => {
+            if (err) return res.status(400).send(String(err))
+            res.json(results)
+        })
+})
+
+app.get('/transferstar', (req, res) => {
+
+    conn.query(`SELECT star, COUNT(*) as Count FROM ordertransaction GROUP BY Star`,
+        (err, results) => {
+            if (err) return res.status(400).send(String(err))
+            res.json(results)
+        })
+})
+
+app.get('/paymentstar', (req, res) => {
+
+    conn.query(`SELECT star, COUNT(*) as Count FROM orderbill GROUP BY Star`,
+        (err, results) => {
+            if (err) return res.status(400).send(String(err))
+            res.json(results)
+        })
+})
+
+app.get('/promostar', (req, res) => {
+
+    conn.query(`SELECT star, COUNT(*) as Count FROM promotionusing GROUP BY Star`,
+        (err, results) => {
+            if (err) return res.status(400).send(String(err))
+            res.json(results)
+        })
+})
+
+app.get('/bankbalance', (req, res) => {
+
+    conn.query(`SELECT Bank , SUM(Balance) as summy FROM accountinfo GROUP BY Bank`,
+        (err, results) => {
+            if (err) return res.status(400).send(String(err))
+            res.json(results)
+        })
+})
+
+app.get('/dailytransaction', (req, res) => {
+
+    conn.query(`SELECT ordertransaction.date ,SUM(ordertransaction.Money) as summy FROM ordertransaction GROUP BY Date`,
+        (err, results) => {
+            if (err) return res.status(400).send(String(err))
+            console.log(results)
+            const data = results.map(e => {
+                return {
+                    // date: String(e.date.getDate()) + '-' + String(e.date.getMonth()) + '-' +String(e.date.getYear()),
+                    date: `${e.date.getDate()}-${e.date.getMonth()+1}-${e.date.getFullYear()}`,
+                    summy: e.summy
+                }
+            })
+            res.json(data)
+        })
+})
+
+app.get('/customerbyages', (req, res) => {
+
+    conn.query(`SELECT FLOOR(DATEDIFF('2019-05-30',DateOfBirth)/365.25) AS DateDiff,COUNT(*) AS MyCount FROM clientinfo GROUP BY DateDiff; `,
+        (err, results) => {
+            if (err) return res.status(400).send(String(err))
+            res.json(results)
+        })
+})
+
+app.get('/bloodtype', (req, res) => {
+
+    conn.query(`SELECT Bloodtype , COUNT(*) as count FROM clientinfo GROUP BY Bloodtype; `,
+        (err, results) => {
+            if (err) return res.status(400).send(String(err))
+            res.json(results)
+        })
+})
+
+app.get('/national', (req, res) => {
+
+    conn.query(`SELECT clientinfo.Nationality ,COUNT(*) as count 
+    FROM clientinfo
+    WHERE clientinfo.Nationality IS NOT NULL
+    GROUP BY clientinfo.Nationality
+    ; `,
+        (err, results) => {
+            if (err) return res.status(400).send(String(err))
+            res.json(results)
+        })
+})
+
+app.get('/notpaid', (req, res) => {
+
+    conn.query(`SELECT type ,COUNT(*) as count
+    FROM billpamentdata
+    WHERE billpamentdata.Status = 'Not paid'
+    GROUP BY type; `,
+        (err, results) => {
+            if (err) return res.status(400).send(String(err))
+            res.json(results)
+        })
+})
+
+app.get('/moneybyaccount', (req, res) => {
+
+    conn.query(`SELECT c.FName,c.LName,b.AccountTypeName,a.bank,SUM(a.Balance) AS AllBalance
+    FROM clientinfo c,accountinfo a,accounttype b
+    WHERE c.clientID = a.clientID AND b.AccountType = a.AccountType
+    GROUP By c.clientid,a.AccountType,a.Bank
+    ORDER BY a.bank
+    ; `,
+        (err, results) => {
+            if (err) return res.status(400).send(String(err))
+            res.json(results)
+        })
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = app
---
+++ FrontEnd/src/views/report.vue
@@ -0,0 +1,350 @@
+<template>
+  <div style="background-color: #ececec; padding: 20px;">
+
+    <a-row :gutter="16">
+      <a-col :span="6">
+        <a-card title="Count of Customer Account" :bordered="false">
+          <table style="width:100%;">
+            <tr>
+              <th>Name</th>
+              <th>Count</th>
+            </tr>
+            <tr v-for="(data, i) in accountCount" :key="i">
+              <td>{{data['FName']}} {{data['LName']}}</td>
+              <td>{{data['CountOfAccount']}}</td>
+            </tr>
+          </table>
+        </a-card>
+      </a-col>
+
+      <a-col :span="6">
+        <a-card title="Number of Closed account" :bordered="false">
+          <h1>{{closecount[0].count}}</h1>
+          <p></p>
+        </a-card>
+      </a-col>
+
+      <a-col :span="6">
+        <a-card title="Churned by Banks" :bordered="false">
+          <table style="width:100%;">
+            <tr>
+              <th>Bank</th>
+              <th>Count</th>
+            </tr>
+            <tr v-for="(data, i) in churncount" :key="i">
+              <td>{{data['NewBank']}}</td>
+              <td>{{data['count']}}</td>
+            </tr>
+          </table>
+        </a-card>
+      </a-col>
+
+      <a-col :span="6">
+        <a-card title="Transferred by Banks" :bordered="false">
+          <table style="width:100%;">
+            <tr>
+              <th>Bank</th>
+              <th>Count</th>
+            </tr>
+            <tr v-for="(data, i) in transfer" :key="i">
+              <td>{{data['Bank']}}</td>
+              <td>{{data['count']}}</td>
+            </tr>
+          </table>
+        </a-card>
+      </a-col>
+    </a-row>
+
+    <a-row :gutter="16" style="margin-top: 15px;">
+      <a-col :span="6">
+        <a-card title="Bill by type" :bordered="false">
+          <table style="width:100%;">
+            <tr>
+              <th>type</th>
+              <th>Count</th>
+            </tr>
+            <tr v-for="(data, i) in billing" :key="i">
+              <td>{{data['Type']}}</td>
+              <td>{{data['count']}}</td>
+            </tr>
+          </table>
+        </a-card>
+      </a-col>
+
+      <a-col :span="6">
+        <a-card title="Stars of transfer" :bordered="false">
+          <table style="width:100%;">
+            <tr>
+              <th>Stars</th>
+              <th>Count</th>
+            </tr>
+            <tr v-for="(data, i) in transferstar" :key="i">
+              <td>{{data['star']}}</td>
+              <td>{{data['Count']}}</td>
+            </tr>
+          </table>
+        </a-card>
+      </a-col>
+
+      <a-col :span="6">
+        <a-card title="Stars of payment" :bordered="false">
+          <table style="width:100%;">
+            <tr>
+              <th>Stars</th>
+              <th>Count</th>
+            </tr>
+            <tr v-for="(data, i) in paymentstar" :key="i">
+              <td>{{data['star']}}</td>
+              <td>{{data['Count']}}</td>
+            </tr>
+          </table>
+        </a-card>
+      </a-col>
+
+      <a-col :span="6">
+        <a-card title="Stars of promotion" :bordered="false">
+          <table style="width:100%;">
+            <tr>
+              <th>stars</th>
+              <th>Count</th>
+            </tr>
+            <tr v-for="(data, i) in promostar" :key="i">
+              <td>{{data['star']}}</td>
+              <td>{{data['Count']}}</td>
+            </tr>
+          </table>
+        </a-card>
+      </a-col>
+    </a-row>
+
+    <a-row :gutter="16"  style="margin-top: 15px;">
+      
+      <a-col :span="6">
+        <a-card title="Balance by Bank" :bordered="false">
+          <table style="width:100%;">
+            <tr>
+              <th>Bank</th>
+              <th>Balance</th>
+            </tr>
+            <tr v-for="(data, i) in bankbalance" :key="i">
+              <td>{{data['Bank']}}</td>
+              <td>{{data['summy']}}</td>
+            </tr>
+          </table>
+        </a-card>
+      </a-col>
+
+      <a-col :span="6">
+        <a-card title="Dailytransaction" :bordered="false">
+          <table style="width:100%;">
+            <tr>
+              <th>Bank</th>
+              <th>Balance</th>
+            </tr>
+            <tr v-for="(data, i) in dailytransaction" :key="i">
+              <td>{{data['date']}}</td>
+              <td>{{data['summy']}}</td>
+            </tr>
+          </table>
+        </a-card>
+      </a-col>
+
+    <a-col :span="6">
+        <a-card title="Customer by Ages" :bordered="false">
+          <table style="width:100%;">
+            <tr>
+              <th>Bank</th>
+              <th>Balance</th>
+            </tr>
+            <tr v-for="(data, i) in customerbyages" :key="i">
+              <td>{{data['DateDiff']}}</td>
+              <td>{{data['MyCount']}}</td>
+            </tr>
+          </table>
+        </a-card>
+      </a-col>
+
+      <a-col :span="6">
+        <a-card title="Customer by Bloodtype" :bordered="false">
+          <table style="width:100%;">
+            <tr>
+              <th>Bloodtype</th>
+              <th>Count</th>
+            </tr>
+            <tr v-for="(data, i) in bloodtype" :key="i">
+              <td>{{data['Bloodtype']}}</td>
+              <td>{{data['count']}}</td>
+            </tr>
+          </table>
+        </a-card>
+      </a-col>
+
+    </a-row>
+    
+    <a-row :gutter="16"  style="margin-top: 15px;">
+
+        <a-col :span="6">
+        <a-card title="Customer by Nationality" :bordered="false">
+          <table style="width:100%;">
+            <tr>
+              <th>Nationality</th>
+              <th>Count</th>
+            </tr>
+            <tr v-for="(data, i) in national" :key="i">
+              <td>{{data['Nationality']}}</td>
+              <td>{{data['count']}}</td>
+            </tr>
+          </table>
+        </a-card>
+      </a-col>
+
+      <a-col :span="6">
+        <a-card title="Not Paid Customer" :bordered="false">
+          <table style="width:100%;">
+            <tr>
+              <th>Type of Bill</th>
+              <th>Count</th>
+            </tr>
+            <tr v-for="(data, i) in notpaid" :key="i">
+              <td>{{data['type']}}</td>
+              <td>{{data['count']}}</td>
+            </tr>
+          </table>
+        </a-card>
+      </a-col>  
+    </a-row>
+
+    <a-row :gutter="16"  style="margin-top: 15px;">
+        <a-col :span="18">
+        <a-card title="Balance by Account" :bordered="false">
+          <table style="width:100%;">
+            <tr>
+              <th>FName</th>
+              <th>LName</th>
+              <th>AccountTypeName</th>
+              <th>Bank</th>
+              <th>AllBalance</th>
+            </tr>
+            <tr v-for="(data, i) in moneybyaccount" :key="i">
+              <td>{{data['FName']}}</td>
+              <td>{{data['LName']}}</td>
+              <td>{{data['AccountTypeName']}}</td>
+              <td>{{data['bank']}}</td>
+              <td>{{data['AllBalance']}}</td>
+            </tr>
+          </table>
+        </a-card>
+      </a-col>
+
+    </a-row>
+  </div>
+</template>
+
+
+<script>
+import api from "@/utils/api";
+export default {
+  name: "reportpage",
+  data() {
+    return {
+      accountCount: {},
+      closecount: {},
+      churncount: {},
+      transfer: {},
+      billing: {},
+      promostar: {},
+      paymentstar: {},
+      transferstar: {},
+      bankbalance:{},
+      dailytransaction:{},
+      customerbyages:{},
+      bloodtype:{},
+      national:{},
+      notpaid:{},
+      moneybyaccount:{}
+    };
+  },
+  mounted() {
+    api()
+      .get("/report/accountcount")
+      .then(({ data }) => {
+        this.accountCount = data;
+      }),
+      api()
+        .get("/report/churncount")
+        .then(({ data }) => {
+          this.churncount = data;
+        }),
+      api()
+        .get("/report/transfer")
+        .then(({ data }) => {
+          this.transfer = data;
+        }),
+      api()
+        .get("/report/billing")
+        .then(({ data }) => {
+          this.billing = data;
+        }),
+      api()
+        .get("/report/transferstar")
+        .then(({ data }) => {
+          this.transferstar = data;
+        }),
+      api()
+        .get("/report/paymentstar")
+        .then(({ data }) => {
+          this.paymentstar = data;
+        }),
+      api()
+        .get("/report/promostar")
+        .then(({ data }) => {
+          this.promostar = data;
+        }),
+        api()
+        .get("/report/bankbalance")
+        .then(({ data }) => {
+          this.bankbalance = data;
+        }),
+        api()
+        .get("/report/dailytransaction")
+        .then(({ data }) => {
+          this.dailytransaction = data;
+        }),
+        api()
+        .get("/report/customerbyages")
+        .then(({ data }) => {
+          this.customerbyages = data;
+        }),
+        api()
+        .get("/report/bloodtype")
+        .then(({ data }) => {
+          this.bloodtype = data;
+        }),
+        api()
+        .get("/report/national")
+        .then(({ data }) => {
+          this.national = data;
+        }),
+        api()
+        .get("/report/notpaid")
+        .then(({ data }) => {
+          this.notpaid = data;
+        }),
+        api()
+        .get("/report/moneybyaccount")
+        .then(({ data }) => {
+          this.moneybyaccount = data;
+        }),
+      api()
+        .get("/report/closecount")
+        .then(({ data }) => {
+          this.closecount = data;
+        });
+  }
+};
+</script>
+<style scoped>
+.ant-card {
+    height: 250px !important;
+}
+</style>
