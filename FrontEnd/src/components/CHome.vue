cef1f95e61bacc6268a58da34e4bc686b630dc00
Taleds <ausupanai@gmail.com>
2019-05-30 13:03:37 +0700

    CHome

--- FrontEnd/src/components/CHome.vue
+++ FrontEnd/src/components/CHome.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="Chome">
-    <h1>{{ msg }}</h1>
 
     <a-carousel :after-change="onChange">
       <div>
@@ -50,13 +49,8 @@
       </div>
     </a-carousel>
     <br />
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
+    <h1>{{ msg }}</h1>
+    
   </div>
 </template>
 
